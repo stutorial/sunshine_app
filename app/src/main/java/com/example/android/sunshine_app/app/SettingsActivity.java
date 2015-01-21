8617c8cad84f15d6aa4a918be2ff79e8be2ec1ff
Santiago <saantii.28@gmail.com>
2015-01-21 16:56:20 -0300

    Add action to setting button in action bar in MainActivity and DetailActivity

--- app/src/main/java/com/example/android/sunshine_app/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine_app/app/SettingsActivity.java
@@ -1,14 +1,11 @@
 package com.example.android.sunshine_app.app;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 public class SettingsActivity extends ActionBarActivity {
 
@@ -17,38 +14,16 @@ public class SettingsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            getFragmentManager().beginTransaction()
                     .add(R.id.container, new SettingsFragment())
                     .commit();
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
-     * A placeholder fragment containing a simple view.
+     * A settings fragment containing a simple view.
      */
-    public static class SettingsFragment extends Fragment {
+    public static class SettingsFragment extends PreferenceFragment {
 
         public SettingsFragment() {
         }
@@ -59,5 +34,7 @@ public class SettingsActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
             return rootView;
         }
+
+
     }
 }
