905f9963668da7103244c13db67aec02173234f9
Santiago <saantii.28@gmail.com>
2015-01-21 14:40:14 -0300

    Add list view layout to the forecast fragment

--- app/src/main/java/com/example/android/sunshine_app/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine_app/app/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.android.sunshine_app.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -59,7 +57,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
             return rootView;
         }
     }
