cf26be81748463a2037187efcffd94071a78f47c
Santiago <saantii.28@gmail.com>
2015-01-21 16:57:08 -0300

    Move settings fragment to a new file

--- app/src/main/java/com/example/android/sunshine_app/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine_app/app/SettingsActivity.java
@@ -1,11 +1,7 @@
 package com.example.android.sunshine_app.app;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class SettingsActivity extends ActionBarActivity {
 
@@ -20,21 +16,4 @@ public class SettingsActivity extends ActionBarActivity {
         }
     }
 
-    /**
-     * A settings fragment containing a simple view.
-     */
-    public static class SettingsFragment extends PreferenceFragment {
-
-        public SettingsFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
-            return rootView;
-        }
-
-
-    }
 }
---
+++ app/src/main/java/com/example/android/sunshine_app/app/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.example.android.sunshine_app.app;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A settings fragment containing a simple view.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+    public SettingsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        return rootView;
+    }
+
+
+}
