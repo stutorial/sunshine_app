412d62cbcffbfb2d6b905d2d34e7cb63e2939a4b
Santiago <saantii.28@gmail.com>
2015-01-23 12:42:27 -0300

    Add test to test weather db helper

--- app/src/main/java/com/example/android/sunshine_app/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine_app/app/data/WeatherDbHelper.java
@@ -12,8 +12,8 @@ import com.example.android.sunshine_app.app.data.WeatherContract.WeatherEntry;
  */
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
+    public static final String DATABASE_NAME = "weather.db";
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "weather.db";
 
     public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
